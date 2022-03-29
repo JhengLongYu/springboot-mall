91f35c5f93511f4c2e5fee35be41df9c5ad2acce
leo <t8102028@gmail.com>
2022-03-29 11:47:52 +0800

    商品功能 - Refactor category，使用Enum 類型

--- src/main/java/com/leoyu/springbootmall/rowmapper/ProductRowMapper.java
+++ src/main/java/com/leoyu/springbootmall/rowmapper/ProductRowMapper.java
@@ -1,5 +1,6 @@
 package com.leoyu.springbootmall.rowmapper;
 
+import com.leoyu.springbootmall.constant.ProductCategory;
 import com.leoyu.springbootmall.model.Product;
 import org.springframework.jdbc.core.RowMapper;
 
@@ -9,17 +10,24 @@ import java.sql.SQLException;
 public class ProductRowMapper implements RowMapper<Product> {
     @Override
     public Product mapRow(ResultSet resultSet, int i) throws SQLException {
-
         Product product = new Product();
+
         product.setProduct_id(resultSet.getInt("product_id"));
         product.setProduct_name(resultSet.getString("product_name"));
-        product.setCategory(resultSet.getString("category"));
+
+        String categoryStr = resultSet.getString("category");
+        ProductCategory category = ProductCategory.valueOf(categoryStr);
+        product.setCategory(category);
+
+//        product.setCategory(ProductCategory.valueOf(resultSet.getString("category")));
+
         product.setImageUrl(resultSet.getString("image_url"));
         product.setPrice(resultSet.getInt("price"));
         product.setStock(resultSet.getInt("stock"));
         product.setDescription(resultSet.getString("description"));
         product.setCreatedDate(resultSet.getTimestamp("created_date"));
         product.setLastModifiedDate(resultSet.getTimestamp("last_modified_date"));
+
         return product;
     }
 }
