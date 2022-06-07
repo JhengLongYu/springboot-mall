2d263c204baa583b787de8d9027f3114d78bdb2c
leo <t8102028@gmail.com>
2022-06-07 13:57:17 +0800

    商品功能 - 測試

--- src/test/java/com/leoyu/springbootmall/controller/ProductControllerTest.java
+++ src/test/java/com/leoyu/springbootmall/controller/ProductControllerTest.java
@@ -1,13 +1,17 @@
 package com.leoyu.springbootmall.controller;
 
-import org.junit.jupiter.api.DisplayName;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.leoyu.springbootmall.constant.ProductCategory;
+import com.leoyu.springbootmall.dto.ProductRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.Matchers.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -22,18 +26,16 @@ public class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void getProducts() {
-    }
+    private ObjectMapper objectMapper = new ObjectMapper();
 
     @Test
     public void getProduct() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .get("/products/{productId}" , 5);
+                .get("/products/{productId}", 1);
 
         mockMvc.perform(requestBuilder)
                 .andDo(print())
-                .andExpect(status().is(200))
+                .andExpect(status().isOk())
                 .andExpect(jsonPath("$.productName", equalTo("蘋果（澳洲）")))
                 .andExpect(jsonPath("$.category", equalTo("FOOD")))
                 .andExpect(jsonPath("$.imageUrl", notNullValue()))
@@ -47,22 +49,211 @@ public class ProductControllerTest {
     @Test
     public void getProduct_notFound() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .get("/products/{productId}" , 200000);
+                .get("/products/{productId}", 20000);
 
         mockMvc.perform(requestBuilder)
-                .andDo(print())
                 .andExpect(status().is(404));
     }
 
+    @Transactional
     @Test
-    void createProduct() {
+    public void createProduct() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+        productRequest.setCategory(ProductCategory.FOOD);
+        productRequest.setImageUrl("http://test.com");
+        productRequest.setPrice(100);
+        productRequest.setStock(2);
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(201))
+                .andExpect(jsonPath("$.productName", equalTo("test food product")))
+                .andExpect(jsonPath("$.category", equalTo("FOOD")))
+                .andExpect(jsonPath("$.imageUrl", equalTo("http://test.com")))
+                .andExpect(jsonPath("$.price", equalTo(100)))
+                .andExpect(jsonPath("$.stock", equalTo(2)))
+                .andExpect(jsonPath("$.description", nullValue()))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+    }
+
+    @Transactional
+    @Test
+    public void createProduct_illegalArgument() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/products")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+    }
+
+    @Transactional
+    @Test
+    public void updateProduct() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+        productRequest.setCategory(ProductCategory.FOOD);
+        productRequest.setImageUrl("http://test.com");
+        productRequest.setPrice(100);
+        productRequest.setStock(2);
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/products/{productId}", 3)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(200))
+                .andExpect(jsonPath("$.productName", equalTo("test food product")))
+                .andExpect(jsonPath("$.category", equalTo("FOOD")))
+                .andExpect(jsonPath("$.imageUrl", equalTo("http://test.com")))
+                .andExpect(jsonPath("$.price", equalTo(100)))
+                .andExpect(jsonPath("$.stock", equalTo(2)))
+                .andExpect(jsonPath("$.description", nullValue()))
+                .andExpect(jsonPath("$.createdDate", notNullValue()))
+                .andExpect(jsonPath("$.lastModifiedDate", notNullValue()));
+    }
+
+    @Transactional
+    @Test
+    public void updateProduct_illegalArgument() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/products/{productId}", 3)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(400));
+
+    }
+
+    @Transactional
+    @Test
+    public void updateProduct_productNotFound() throws Exception {
+        ProductRequest productRequest = new ProductRequest();
+        productRequest.setProductName("test food product");
+        productRequest.setCategory(ProductCategory.FOOD);
+        productRequest.setImageUrl("http://test.com");
+        productRequest.setPrice(100);
+        productRequest.setStock(2);
+
+        String json = objectMapper.writeValueAsString(productRequest);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .put("/products/{productId}", 20000)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(json);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(404));
+    }
+
+    @Transactional
+    @Test
+    public void deleteProduct_deleteExistingProduct() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/products/{productId}", 5);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(204));
+    }
+
+    @Transactional
+    @Test
+    public void deleteProduct_deleteNonExistingProduct() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .delete("/products/{productId}", 20000);
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().is(204));
     }
 
     @Test
-    void updateProduct() {
+    public void getProducts() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products");
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit", notNullValue()))
+                .andExpect(jsonPath("$.offset", notNullValue()))
+                .andExpect(jsonPath("$.total", notNullValue()))
+                .andExpect(jsonPath("$.results", hasSize(5)));
     }
 
     @Test
-    void deleteProduct() {
+    public void getProducts_filtering() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products")
+                .param("search", "B")
+                .param("category", "CAR");
+
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit", notNullValue()))
+                .andExpect(jsonPath("$.offset", notNullValue()))
+                .andExpect(jsonPath("$.total", notNullValue()))
+                .andExpect(jsonPath("$.results", hasSize(2)));
+    }
+
+    @Test
+    public void getProducts_sorting() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products")
+                .param("orderBy", "price")
+                .param("sort", "desc");
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit", notNullValue()))
+                .andExpect(jsonPath("$.offset", notNullValue()))
+                .andExpect(jsonPath("$.total", notNullValue()))
+                .andExpect(jsonPath("$.result", hasSize(5)))
+                .andExpect(jsonPath("$.result[0].productId", equalTo(6)))
+                .andExpect(jsonPath("$.result[1].productId", equalTo(5)))
+                .andExpect(jsonPath("$.result[2].productId", equalTo(7)))
+                .andExpect(jsonPath("$.result[3].productId", equalTo(4)))
+                .andExpect(jsonPath("$.result[4].productId", equalTo(2)));
+    }
+
+    @Test
+    public void getProducts_pagination() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/products")
+                .param("limit", "2")
+                .param("offset", "2");
+
+        mockMvc.perform(requestBuilder)
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.limit", notNullValue()))
+                .andExpect(jsonPath("$.offset", notNullValue()))
+                .andExpect(jsonPath("$.total", notNullValue()))
+                .andExpect(jsonPath("$.results", hasSize(2)))
+                .andExpect(jsonPath("$.results[0].productId", equalTo(5)))
+                .andExpect(jsonPath("$.results[1].productId", equalTo(4)));
     }
 }
